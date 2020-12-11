30ff0e113c5c46071fbd54644f44a91ac770444e
bmtalotta <57196623+bmtalotta@users.noreply.github.com>
2020-12-10 16:03:41 -0900

    setting up the files

---
+++ convexhull.hpp
@@ -0,0 +1,9 @@
+//convexhull.hpp
+// Ben Talotta
+// For CS 411 Fall 2020 HW 7
+
+#ifndef FILE_CONVEXHULL_H_INCLUDED
+#define FILE_CONVEXHULL_H_INCLUDED
+
+#endif //#ifndef FILE_CONVEXHULL_H_INCLUDED
+
